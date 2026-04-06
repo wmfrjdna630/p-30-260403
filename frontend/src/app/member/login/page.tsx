c6102aeae33c0be3297e9df253d80389526df22f
wmfrjdna630 <111439483+wmfrjdna630@users.noreply.github.com>
2026-04-06 10:25:23 +0900

    front login page

---
+++ frontend/src/app/member/login/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { fetchApi } from "@/lib/client";
+import { useRouter } from "next/navigation";
+
+export default function Write() {
+
+    const router = useRouter();
+
+    const onSubmitHandler = (e: any) => {
+        e.preventDefault();;
+        const form = e.target;
+        const username = form.username;
+        const password = form.password;
+
+        if (username.value.length === 0) {
+            alert("아이디를 입력해주세요.");
+            username.focus();
+            return;
+        }
+
+        if (password.value.length === 0) {
+            alert("비밀번호를 입력해주세요.");
+            password.focus();
+            return;
+        }
+
+        // db에 저장.
+        fetchApi(`/api/v1/members/login`, {
+            method: "POST",
+            body: JSON.stringify({
+                "username": username.value,
+                "password": password.value
+            })
+        })
+            .then(rs => {
+                alert(rs.msg);
+                router.replace(`/`)
+            })
+            .catch((errMsg) => {
+                alert(errMsg);
+            })
+    }
+
+    return (
+        <>
+            <h1>로그인</h1>
+
+            <form action="" onSubmit={onSubmitHandler} className="flex flex-col gap-4">
+                <input type="text" name="username" className="border-1 rounded p-2" placeholder="아이디를 입력해주세요" />
+                <input type="password" name="password" className="border-1 rounded p-2" placeholder="비밀번호를 입력해주세요." />
+                <button className="bg-blue-500 text-white p-2 rounded" type="submit">
+                    로그인
+                </button>
+            </form>
+        </>
+    )
+}
